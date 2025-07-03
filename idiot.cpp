3a1b63d3783ac79cebdadb1bbf322923d6ce7b88
Uranus_18C5E <mc2748246086@outlook.com>
2025-07-03 22:34:04 +0800

    Add files via upload

---
+++ idiot.cpp
@@ -0,0 +1,13 @@
+#include<iostream>
+#include<windows.h>
+
+int main(){
+    while(64)
+    {
+        MessageBox(NULL,"YOU ARE AN IDIOT!!!","£Ù£Ï£Õ¡¡£Á£Ò£Å¡¡£Á£Î¡¡£É£Ä£É£Ï£Ô£¡£¡£¡",MB_OK|MB_ICONWARNING);
+    }
+	
+    
+//don't start this file!!!
+    return 0;
+}
