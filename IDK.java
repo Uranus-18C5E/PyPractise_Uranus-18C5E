0b41c4dfe77d067edae214570a965ed5666f0a9b
Uranus_18C5E <mc2748246086@outlook.com>
2025-07-20 14:04:45 +0800

    Add files via upload

---
+++ IDK.java
@@ -0,0 +1,14 @@
+public class IDK{
+    public static void main(String[] args){
+        int idk=2147483647;
+        idk=idk+1;
+        System.out.println(idk);
+        System.out.println("Without\"%d\"?!");
+        long idk1=2147483647;
+        idk1++;
+        System.out.println(idk1);
+        System.out.println("Support ++ and long!?");
+        System.out.printf("Also support printf?!\nBut no cout&& long long");
+        //But no cout :(
+    }
+}
